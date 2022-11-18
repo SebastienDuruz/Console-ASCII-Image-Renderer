7b321cdedb8e5a1446b630637102e3c5a48456e8
Sébastien Duruz <sebastienduruz@protonmail.com>
2022-11-18 07:59:15 +0100

    Minor changes

--- ConsoleIMGRenderer/ConsoleIMGRenderer/Program.cs
+++ ConsoleIMGRenderer/ConsoleIMGRenderer/Program.cs
@@ -85,7 +85,7 @@ namespace ConsoleIMGRenderer
             }
 
             Console.Clear();
-            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.BackgroundColor = ConsoleColor.Black;
             Console.CursorVisible = false;
 
